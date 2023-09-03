e52e1aa24d39b68aad6e83d6a8d77f9690cfad4a
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2023-09-03 18:16:58 +0300

    redhat sso addition

--- Logger/src/main/java/main/security/SecurityConfig.java
+++ Logger/src/main/java/main/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package main.security;
-import lombok.RequiredArgsConstructor;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,9 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 
 
 @Configuration
